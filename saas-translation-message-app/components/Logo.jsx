ab647a87c76b76256517fd7bce60696a0550a78d
BoyanK95 <boyan_k_95@abv.bg>
2024-01-07 21:18:12 +0200

    feat: created Logo component

--- saas-translation-message-app/components/Logo.jsx
+++ saas-translation-message-app/components/Logo.jsx
@@ -1,7 +1,27 @@
+import LogoImage from "@logos/white-chat-logo.gif";
+import StaticLogoImage from '@logos/logo-chat-static.png'
 import Link from "next/link";
+import { AspectRatio } from "./ui/aspect-ratio";
+import Image from "next/image";
 
 function Logo() {
-  return <Link href="/">Go home</Link>;
+  return (
+    <Link href="/">
+      <div className="flex items-center w-72 h-14">
+        <AspectRatio
+          ratio={16 / 9}
+          className="flex items-center justify-center"
+        >
+          <Image
+            src={LogoImage}
+            alt="white-logo-image"
+            priority
+            className="roundef-full dark:filter dark:invert"
+          />
+        </AspectRatio>
+      </div>
+    </Link>
+  );
 }
 
 export default Logo;
