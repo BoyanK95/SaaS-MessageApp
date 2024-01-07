924676111cedebb9bb489a48964e4d7d2ceb0ced
BoyanK95 <boyan_k_95@abv.bg>
2024-01-07 22:30:32 +0200

    style: add header nav and logo text styles

--- saas-translation-message-app/components/Header.jsx
+++ saas-translation-message-app/components/Header.jsx
@@ -1,9 +1,17 @@
-import Logo from './Logo'
+import Logo from "./Logo";
 
 function Header() {
   return (
-    <header>
-      <nav><Logo /></nav>
+    <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
+      <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
+        <Logo />
+        <div>
+          {/* Language selet */}
+
+          {/* Seassion && ... */}
+        </div>
+      </nav>
+      {/* Upgrade Banner */}
     </header>
   );
 }
