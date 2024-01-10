3072a72cc9e86b71ecf4c17c959fda1196dcfe94
BoyanK95 <boyan_k_95@abv.bg>
2024-01-10 23:57:40 +0200

    feat: add session to Header component

--- saas-translation-message-app/components/ui/Header.jsx
+++ saas-translation-message-app/components/ui/Header.jsx
@@ -1,8 +1,13 @@
 import Logo from "./Logo";
 import DarkModeToggle from "./DarkModeToggle";
 import UserButton from "./UserButton";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth";
 
-function Header() {
+async function Header() {
+  const session = await getServerSession(authOptions);
+  console.log("session", session);
+
   return (
     <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
       <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
