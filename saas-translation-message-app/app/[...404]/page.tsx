a90ab7e56a5b75d7c5d753317e8cdae3e9eb2f8b
BoyanK95 <boyan_k_95@abv.bg>
2024-01-13 00:03:29 +0200

    refactore: linted

--- saas-translation-message-app/app/[...404]/page.tsx
+++ saas-translation-message-app/app/[...404]/page.tsx
@@ -7,7 +7,9 @@ export default function NotFoundPage() {
     <>
       <main className="grid min-h-full place-items-center bg-white dark:bg-gray-800 px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
-          <p className="font-semibold text-indigo-600 dark:text-red-300 text-3xl">404</p>
+          <p className="font-semibold text-indigo-600 dark:text-red-300 text-3xl">
+            404
+          </p>
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-5xl">
             Page not found
           </h1>
