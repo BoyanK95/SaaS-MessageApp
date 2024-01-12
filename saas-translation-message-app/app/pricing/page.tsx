2e7ca218146709d51a7d77975c2c8025a7791af1
BoyanK95 <boyan_k_95@abv.bg>
2024-01-12 12:05:03 +0200

    feat: add Pricing page top text content

--- saas-translation-message-app/app/pricing/page.tsx
+++ saas-translation-message-app/app/pricing/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
-const Pricing = () => {
+const PricingPage = () => {
   return (
-    <div>Pricing</div>
-  )
-}
+    <div className="isolate overflow-hidden bg-gray-900">
+      <div className="mx-auto max-w-7xl pb-96 pt-24 text-center sm:pt-32 lg:px-8">
+        <div className="mx-auto max-w-4xl">
+          <h2 className="text-base font-semibold leading-7 text-indigo-300">
+            Pricing
+          </h2>
+          <p className="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">
+            The right price for you,{" "}
+            <br className="hidden sm:inline lg:hidden" />
+            whoever you are
+          </p>
+        </div>
+        <div className="relative mt-6">
+          <p className="mx-auto max-w-2xl text-lg text-white/60">
+            Where 99% sure we have a plan to match your needs
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Pricing
+export default PricingPage;
