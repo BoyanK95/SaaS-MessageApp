b52a9aa9716fdc0c8668016553a2828132e98321
BoyanK95 <boyan_k_95@abv.bg>
2024-01-13 02:56:24 +0200

    feat: create initial button with styles

--- saas-translation-message-app/components/CheckoutButton.tsx
+++ saas-translation-message-app/components/CheckoutButton.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const CheckoutButton = () => {
   return (
-    <div>
-        {/* TODO if subsribed show the user is subscribed */}
+    <div className="flex flex-col space-y-2">
+      {/* TODO if subsribed show the user is subscribed */}
       <button className="mt-10 rounded-md bg-indigo-500 py-2.5 px-3.5 text-sm font-semibold text-white dark:text-whte hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-indigo-600 disabled:bg-gray-500">
         Sign Up
       </button>
