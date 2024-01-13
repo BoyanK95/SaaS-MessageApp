2bf83168b95016dbb2ab6c58d1b904d1df4da228
BoyanK95 <boyan_k_95@abv.bg>
2024-01-13 02:56:03 +0200

    feat: create CheckoutButton

--- saas-translation-message-app/components/CheckoutButton.tsx
+++ saas-translation-message-app/components/CheckoutButton.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const CheckoutButton = () => {
   return (
-    <div>CheckoutButton</div>
-  )
-}
+    <div>
+        {/* TODO if subsribed show the user is subscribed */}
+      <button className="mt-10 rounded-md bg-indigo-500 py-2.5 px-3.5 text-sm font-semibold text-white dark:text-whte hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-indigo-600 disabled:bg-gray-500">
+        Sign Up
+      </button>
+    </div>
+  );
+};
 
-export default CheckoutButton
+export default CheckoutButton;
