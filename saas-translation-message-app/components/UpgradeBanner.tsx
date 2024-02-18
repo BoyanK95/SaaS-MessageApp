41edcd1f8f30ad26d28c4c3a844d87daabe54dfe
BoyanK95 <boyan_k_95@abv.bg>
2024-02-18 20:39:13 +0200

    feat: render UpgradeBanner if we dont have subscription

--- saas-translation-message-app/components/UpgradeBanner.tsx
+++ saas-translation-message-app/components/UpgradeBanner.tsx
@@ -3,21 +3,23 @@
 import { useSubscriptionStore } from "@/store/store";
 import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const UpgradeBanner = () => {
   const subscription = useSubscriptionStore((state) => state.subscription);
   const router = useRouter();
 
   console.log(subscription);
-
-  return (
-    <Button
-      className="w-full rounded-none bg-gradient-to-r from-purple-300 to bg-red-300 text-lg font-bold text-center text-slate-900 hover:text-white dark:hover:text-purple-900 px-5 py-2 transition-all"
-      onClick={() => router.push("/register")}
-    >
-      Upgrade to Pro to unlock all features
-    </Button>
-  );
+  if (!subscription) {
+    return (
+      <Button
+        className="w-full rounded-none bg-gradient-to-r from-purple-300 to bg-red-300 text-lg font-bold text-center text-slate-900 hover:text-white dark:hover:text-purple-900 px-5 py-2 transition-all"
+        onClick={() => router.push("/register")}
+      >
+        Upgrade to Pro to unlock all features
+      </Button>
+    );
+  }
 };
 
 export default UpgradeBanner;
