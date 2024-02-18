6857ee6812ab6be3cf894e5d081ae5e4228f0398
BoyanK95 <boyan_k_95@abv.bg>
2024-02-18 20:42:17 +0200

    refactor: UpgradeBanner

--- saas-translation-message-app/components/UpgradeBanner.tsx
+++ saas-translation-message-app/components/UpgradeBanner.tsx
@@ -9,17 +9,18 @@ const UpgradeBanner = () => {
   const subscription = useSubscriptionStore((state) => state.subscription);
   const router = useRouter();
 
-  console.log(subscription);
-  if (!subscription) {
-    return (
-      <Button
-        className="w-full rounded-none bg-gradient-to-r from-purple-300 to bg-red-300 text-lg font-bold text-center text-slate-900 hover:text-white dark:hover:text-purple-900 px-5 py-2 transition-all"
-        onClick={() => router.push("/register")}
-      >
-        Upgrade to Pro to unlock all features
-      </Button>
-    );
+  if (subscription) {
+    return null;
   }
+
+  return (
+    <Button
+      className="w-full rounded-none bg-gradient-to-r from-purple-300 to bg-red-300 text-lg font-bold text-center text-slate-900 hover:text-white dark:hover:text-purple-900 px-5 py-2 transition-all"
+      onClick={() => router.push("/register")}
+    >
+      Upgrade to Pro to unlock all features
+    </Button>
+  );
 };
 
 export default UpgradeBanner;
