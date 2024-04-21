5ab72598d67b91678462f13843c62a712e103197
BoyanK95 <boyan_k_95@abv.bg>
2024-04-21 23:45:54 +0300

    feat: create server action for managing subscription

---
+++ saas-translation-message-app/actions/generatePortalLink.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import { authOptions } from "@/auth";
+import { getServerSession } from "next-auth";
+import { Stripe } from "stripe";
+import { headers } from "next/headers";
+import { adminDb } from "@/firebase-admin";
+import { redirect } from "next/navigation";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2023-10-16",
+});
+
+export async function generatePortalLink(params: any) {
+  const session = await getServerSession(authOptions);
+  const host = headers().get("host");
+
+  // TODO: throw error instead of console.log after creating error.tsx page
+  if (!session?.user.id) return console.log("No user Id found!!!");
+
+  const {
+    user: { id },
+  } = session;
+
+  const returnUrl =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/register"
+      : `https://${host}/register`;
+
+  const doc = await adminDb.collection("customers").doc(id).get();
+
+  if (!doc.data) {
+    //TODO throw error instead of console.log after creating error.tsx page
+    return console.log("No custumer found: ", id);
+  }
+
+  const stripeId = doc.data()!.stripeId;
+
+  const stripeSession = await stripe.billingPortal.sessions.create({
+    customer: stripeId,
+    return_url: returnUrl,
+  });
+
+  redirect(stripeSession.url);
+}
--- saas-translation-message-app/components/ManageAccountButton.tsx
+++ saas-translation-message-app/components/ManageAccountButton.tsx
@@ -1,11 +1,13 @@
+import { generatePortalLink } from "@/actions/generatePortalLink";
 import React from "react";
 
 const ManageAccountButton = () => {
   return (
-    <form className="mt-10 text-center rounded-md bg-indigo-500 py-2.5 px-3.5 text-sm font-semibold text-white dark:text-whte hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-indigo-600 disabled:bg-gray-500" action="">
-      <button type="submit">
-        Manage Account
-      </button>
+    <form
+      action={generatePortalLink}
+      className="mt-10 text-center rounded-md bg-indigo-500 py-2.5 px-3.5 text-sm font-semibold text-white dark:text-whte hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-indigo-600 disabled:bg-gray-500"
+    >
+      <button type="submit">Manage Account</button>
     </form>
   );
 };
