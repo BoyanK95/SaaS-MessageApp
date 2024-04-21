bc05ea998d158d4fe9d4f953479c810338cea136
BoyanK95 <boyan_k_95@abv.bg>
2024-04-22 00:28:45 +0300

    style: cursor wait for loading btn

--- saas-translation-message-app/components/LoadingButton.tsx
+++ saas-translation-message-app/components/LoadingButton.tsx
@@ -5,7 +5,7 @@ import { RotatingLines } from "react-loader-spinner";
 
 const LoadingButton = () => {
   return (
-    <div className="flex justify-center gap-5">
+    <div className="flex justify-center gap-5 cursor-wait">
     <RotatingLines width="18" strokeColor="white" />
     <p className="animate-pulse">Loading...</p>
   </div>
