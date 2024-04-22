e08719090c6f0c5b56050468e3282788d10a90b5
BoyanK95 <boyan_k_95@abv.bg>
2024-04-22 20:46:12 +0300

    feat: create initial structure of chat pages

---
+++ saas-translation-message-app/app/chat/[chatId]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ChatPage = () => {
+  return (
+    <>
+        {/* Admin controls */}
+        {/* Chat members badge */}
+        {/* Chat messages */}
+        {/* Chat Inout */}
+    </>
+  )
+}
+
+export default ChatPage
---
+++ saas-translation-message-app/app/chat/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+type ChatsProps = {
+  params: {};
+  searchParams: {
+    error: string;
+  };
+};
+
+const ChatsPage = ({ searchParams: { error } }: ChatsProps) => {
+  return (
+    <div>
+      {/* Chat Permisson cat */}
+
+      {/* ChatList */}
+    </div>
+  );
+};
+
+export default ChatsPage;
