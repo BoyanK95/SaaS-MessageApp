1324e270a4d22c2e3d4add25c07926f231db9349
BoyanK95 <boyan_k_95@abv.bg>
2024-04-27 17:37:14 +0300

    feat: create chat db,  createNewChat initial functionallity

--- saas-translation-message-app/components/CreateChatButton.tsx
+++ saas-translation-message-app/components/CreateChatButton.tsx
@@ -8,6 +8,8 @@ import { useSession } from "next-auth/react";
 import { useToast } from "./ui/use-toast";
 import LoadingButton from "./LoadingButton";
 import { v4 as uuidv4 } from "uuid";
+import { serverTimestamp, setDoc } from "firebase/firestore";
+import { addChatRef } from "@/lib/converters/ChatMembers";
 
 const CreateChatButton = ({ isLarge }: { isLarge?: boolean }) => {
   const [loading, setLoading] = useState(false);
@@ -18,17 +20,45 @@ const CreateChatButton = ({ isLarge }: { isLarge?: boolean }) => {
 
   const createNewChat = async () => {
     if (!session?.user.id) return;
-
     setLoading(true);
     toast({
       title: "Creating a new chat...",
       description:
         "This may take a few seconds, while we create a new chat for you...",
-      duration: 3000,
+      duration: 2000,
     });
     // TODO: check if subscription is active and limit chat depending on it
     const chatId = uuidv4();
-    router.push("/chat/abs");
+    await setDoc(addChatRef(chatId, session.user.id), {
+      userId: session.user.id,
+      email: session.user.email as string,
+      timestamp: serverTimestamp(),
+      isAdmin: true,
+      chatId: chatId,
+      image: session.user.image || "",
+    })
+      .then(() => {
+        toast({
+          title: "Success!",
+          description: "You can now start chatting with your friends!",
+          className: "bg-green-600 text-white",
+          duration: 3000,
+        });
+        router.push(`chat/${chatId}`);
+      })
+      .catch((error) => {
+        console.error(error);
+        
+        toast({
+          title: "Error!",
+          description: "Something went wrong, please try again later.",
+          variant: "destructive",
+          duration: 3000,
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
   if (isLarge) {
