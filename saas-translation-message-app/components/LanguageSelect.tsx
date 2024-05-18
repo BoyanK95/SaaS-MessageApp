5a36471c5876e826d1a1f0ee9e7f7ed9351edcd4
BoyanK95 <boyan_k_95@abv.bg>
2024-05-18 03:28:08 +0300

    feat: install Select shadc component use it in LanguageSelect to select language

--- saas-translation-message-app/components/LanguageSelect.tsx
+++ saas-translation-message-app/components/LanguageSelect.tsx
@@ -1,7 +1,20 @@
 "use client";
 
-import { useLanguageStore, useSubscriptionStore } from "@/store/store";
+import {
+  LanguageSuported,
+  LanguageSupportedMap,
+  useLanguageStore,
+  useSubscriptionStore,
+} from "@/store/store";
 import { usePathname } from "next/navigation";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import LoadingButton from "./LoadingButton";
 
 const LanguageSelect = () => {
   const [language, setLanguage, getLanguages, getNotSuportedLanguages] =
@@ -12,12 +25,35 @@ const LanguageSelect = () => {
       state.getNotSuportedLanguages,
     ]);
   const pathName = usePathname();
-  const isChatPage = pathName.includes("chat");
+  const isChatPage = pathName.includes("/chat");
 
   const subscription = useSubscriptionStore((state) => state.subscription);
   const hasSubscription = subscription?.status === "active";
 
-  return isChatPage && <div>LanguageSelect</div>;
+  return (
+    isChatPage && (
+      <div>
+        <Select onValueChange={(value: LanguageSuported) => setLanguage(value)}>
+          <SelectTrigger className="w-[150px] text-black dark:text-white">
+            <SelectValue placeholder={LanguageSupportedMap[language]} />
+          </SelectTrigger>
+          <SelectContent>
+            {subscription === undefined ? (
+              <LoadingButton />
+            ) : (
+              <>
+                {getLanguages(hasSubscription).map((language) => (
+                  <SelectItem key={language} value={language}>
+                    {LanguageSupportedMap[language]}
+                  </SelectItem>
+                ))}
+              </>
+            )}
+          </SelectContent>
+        </Select>
+      </div>
+    )
+  );
 };
 
 export default LanguageSelect;
