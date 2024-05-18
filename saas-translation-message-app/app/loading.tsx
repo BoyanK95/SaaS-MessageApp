a388d42de8f38002ae282b1471bf9030e36d9a55
BoyanK95 <boyan_k_95@abv.bg>
2024-05-19 02:00:51 +0300

    feat: create loading page

---
+++ saas-translation-message-app/app/loading.tsx
@@ -0,0 +1,12 @@
+import LoadingButton from "@/components/LoadingButton";
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center p-10">
+      <LoadingButton textCss="text-2xl font-semibold text-center items-center" />
+    </div>
+  );
+};
+
+export default Loading;
--- saas-translation-message-app/components/LoadingButton.tsx
+++ saas-translation-message-app/components/LoadingButton.tsx
@@ -1,15 +1,14 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
 import { RotatingLines } from "react-loader-spinner";
 
-
-const LoadingButton = () => {
+const LoadingButton = ({ textCss }: { textCss?: string }) => {
   return (
     <div className="flex justify-center gap-5 cursor-wait">
-    <RotatingLines width="18" strokeColor="white" />
-    <p className="animate-pulse">Loading...</p>
-  </div>
-  )
-}
+      <RotatingLines width="18" strokeColor="white" />
+      <p className={`animate-pulse ${textCss}`}>Loading...</p>
+    </div>
+  );
+};
 
-export default LoadingButton
+export default LoadingButton;
