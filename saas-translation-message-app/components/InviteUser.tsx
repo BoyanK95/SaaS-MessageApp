801160e55b669b71721b50eb30707e9a8d3f1987
BoyanK95 <boyan_k_95@abv.bg>
2024-06-21 00:22:32 +0300

    feat: create initial InviteUser structure till form

--- saas-translation-message-app/components/InviteUser.tsx
+++ saas-translation-message-app/components/InviteUser.tsx
@@ -1,9 +1,78 @@
-import React from 'react'
+"use client";
 
-const InviteUser = ({chatId}: {chatId: string}) => {
+import { useSession } from "next-auth/react";
+import React, { useState } from "react";
+import { z } from "zod";
+import { useToast } from "./ui/use-toast";
+import useAdminId from "@/hooks/useAdminId";
+import { useSubscriptionStore } from "@/store/store";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "./ui/button";
+import { PlusCircleIcon } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Form } from "./ui/form";
+
+const formSchema = z.object({
+  email: z.string().email("Please enter a valid email address"),
+});
+
+const InviteUser = ({ chatId }: { chatId: string }) => {
+  const { data: session } = useSession();
+  const { toast } = useToast();
+  const adminId = useAdminId({ chatId });
+  const subscription = useSubscriptionStore((state) => state.subscription);
+  const router = useRouter();
+
+  const [open, setOpen] = useState<boolean>(false);
+  const [openInviteLink, setOpenInviteLink] = useState<boolean>(false);
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
   return (
-    <></>
-  )
-}
+    adminId === session?.user.id && (
+      <>
+        <Dialog open={open} onOpenChange={setOpen}>
+          <DialogTrigger asChild>
+            <Button>
+              <PlusCircleIcon className="mr-1" />
+              Add User to Chat
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-md">
+            <DialogHeader>
+              <DialogTitle>Add User to Chat</DialogTitle>
+              <DialogDescription>
+                Simply enter another users email address to invite them to this
+                chat!{" "}
+                <span className="text-indigo-600 font-bold">
+                  (Note: they must be registered!)
+                </span>
+              </DialogDescription>
+            </DialogHeader>
+            <Form {...form}>
+              <form>
+                
+              </form>
+            </Form>
+          </DialogContent>
+        </Dialog>
+      </>
+    )
+  );
+};
 
-export default InviteUser
+export default InviteUser;
